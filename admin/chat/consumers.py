29e537c86536fdc91364ab392620dcd93d1c4ecf
Julien Capellari <julien@capellari.net>
2018-09-09 11:49:09 +0200

    async chat

--- admin/chat/consumers.py
+++ admin/chat/consumers.py
@@ -2,8 +2,7 @@
 from asgiref.sync import async_to_sync
 import json
 
-from channels.generic.websocket import WebsocketConsumer
-
+from channels.generic.websocket import WebsocketConsumer, AsyncWebsocketConsumer
 
 # Consumer
 class ChatConsumer(WebsocketConsumer):
@@ -44,5 +43,46 @@ class ChatConsumer(WebsocketConsumer):
         message = event['message']
 
         self.send(text_data=json.dumps({
+            'message': message
+        }))
+
+class AsyncChatConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
+        # Join room
+        self.room_name = self.scope['url_route']['kwargs']['room_name']
+        self.room_group_name = "chat_{}".format(self.room_name)
+
+        await self.channel_layer.group_add(
+            self.room_group_name,
+            self.channel_name
+        )
+
+        # Accept connexion
+        await self.accept()
+
+    async def disconnect(self, close_code):
+        # Leave room
+        await self.channel_layer.group_discard(
+            self.room_group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        # Send message to room
+        text_data_json = json.loads(text_data)
+        message = text_data_json['message']
+
+        await self.channel_layer.group_send(
+            self.room_group_name,
+            {
+                'type': 'chat_message',
+                'message': message
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+
+        await self.send(text_data=json.dumps({
             'message': message
         }))
--- admin/chat/routing.py
+++ admin/chat/routing.py
@@ -3,6 +3,7 @@ from django.urls import path
 
 from . import consumers
 
+# URLs
 websocket_urlpatterns = [
-    path(r'ws/chat/<room_name>/', consumers.ChatConsumer),
+    path(r'ws/chat/<room_name>/', consumers.AsyncChatConsumer, name = "ws_room"),
 ]
