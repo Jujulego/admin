56d00a5e92b6e80c7dabffb141ca97c28357c1de
Julien Capellari <julien@capellari.net>
2019-01-12 18:14:24 +0100

    add: start, reload and stop vassal from admin site

--- admin_api/emperor/admin.py
+++ admin_api/emperor/admin.py
@@ -1,14 +1,22 @@
 # Importations
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.contrib.admin.utils import quote
+from django.urls import reverse
+from django.utils import timezone
+from django.utils.html import format_html
+from django.utils.http import urlquote
 
+from utils import AdminUtilsMixin
 from .models import Vassal
 
 # Register your models here.
 @admin.register(Vassal)
-class VassalAdmin(admin.ModelAdmin):
+class VassalAdmin(admin.ModelAdmin, AdminUtilsMixin):
     # Liste
     list_display = ("nom", "actif")
     list_filter = ("actif",)
+
+    actions = ("start", "reload", "stop")
 
     # Edition
     fieldsets = (
@@ -21,3 +29,49 @@ class VassalAdmin(admin.ModelAdmin):
     )
 
     readonly_fields = ("date_modif",)
+
+    # Actions
+    def start(self, request, qs):
+        # Filtrage
+        qs = qs.filter(actif=False)
+
+        # Message
+        if qs.count() == 1:
+            messages.info(request, format_html('Lancement de {}', self.link_repr(request, qs.first())))
+
+        elif qs.count() > 1:
+            messages.info(request, "Lancement de {} vassaux".format(qs.count()))
+
+        # Activation !
+        qs.update(actif=True, date_modif=timezone.now())
+
+    start.short_description = "Lance les vassaux"
+
+    def stop(self, request, qs):
+        # Filtrage
+        qs = qs.filter(actif=True)
+
+        # Message
+        if qs.count() == 1:
+            messages.info(request, format_html('Arrêt de {}', self.link_repr(request, qs.first())))
+
+        elif qs.count() > 1:
+            messages.info(request, "Arrêt de {} vassaux".format(qs.count()))
+
+        # Activation !
+        qs.update(actif=False, date_modif=timezone.now())
+
+    stop.short_description = "Arrête les vassaux"
+
+    def reload(self, request, qs):
+        # La mise à jour de la date force le relancement
+        qs.update(date_modif=timezone.now())
+
+        # Message
+        if qs.count() == 1:
+            messages.info(request, format_html('Relancement de {}', self.link_repr(request, qs.first())))
+
+        elif qs.count() > 1:
+            messages.info(request, "Relancement de {} vassaux".format(qs.count()))
+
+    reload.short_description = "Relance les vassaux"
